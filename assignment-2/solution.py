2c93dc03327ef8de48c4492c4eb7af1b94115a85
Rutger Hofman <rutger@cs.vu.nl>
2018-06-07 17:22:38 +0200

    Refactor Assignment2

---
+++ assignment-2/solution.py
@@ -0,0 +1,34 @@
+"""
+We truthfully declare:
+- to have contributed approximately equally to this assignment [if this is not true, modify this sentence to disclose individual contributions so we can grade accordingly]
+- that we have neither helped other students nor received help from other students
+- that we provided references for all code that is not our own
+
+Name Student 1 email@vu.nl
+Name Student 2 email@vu.nl
+"""
+
+def read_csv_data(filenames):
+    None
+    
+    
+def to_mongodb(df):
+    None
+
+
+def read_mongodb(filter,sort):
+    None
+
+
+if __name__ == '__main__':
+    # this code block is run if you run solution.py (instead of run_solution.py)
+    # it is convenient for debugging
+
+    df = read_csv_data(["hue_upload.csv","hue_upload2.csv"])
+    # to_mongodb(df)
+    # read_mongodb({},'_id')
+
+
+
+
+
